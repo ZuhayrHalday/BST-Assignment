3d8e5a14a5e1c965e0ec2426caaa81d31fe17889
ZuhayrHalday <halday.zuhayr@gmail.com>
2024-03-08 21:09:16 +0200

    Defined a class for each Node in a BST

--- src/GenericsKbBSTApp.java
+++ src/GenericsKbBSTApp.java
@@ -1,3 +1,14 @@
+class Node{
+    String data;
+    Node left;
+    Node right;
+
+    public Node(String data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
 public class GenericsKbBSTApp {
     
 }
