f4320f301de0f5353eddd22b27f70a55e41c493c
ZuhayrHalday <halday.zuhayr@gmail.com>
2024-03-08 18:17:23 +0200

    Updated LineCount function to read each line of an imported file

--- src/GenericsKbArrayApp.java
+++ src/GenericsKbArrayApp.java
@@ -1,10 +1,25 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class GenericsKbArrayApp {
     private static String[] arrFile;
     
+    public static int LineCount(String fileName){
+        int numLines = 0;
+        String file = fileName;
+
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        while (bufferedReader.readLine() != null){
+            numLines++;
+        }
+        
+        return numLines;
+    }
+
 }
