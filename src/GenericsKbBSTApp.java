2b990b32f1748b34db0aa5e557fc60658de8f4a4
ZuhayrHalday <halday.zuhayr@gmail.com>
2024-03-08 21:18:22 +0200

    Implemented a BST Insert algorithm from Data Structure lectrues

--- src/GenericsKbBSTApp.java
+++ src/GenericsKbBSTApp.java
@@ -16,6 +16,27 @@ class BST{
     public BST(){
         root = null;
     }
+
+    public void insert(String data){
+        root = insertBST(root, data);
+    }
+
+    private Node insertBST(Node root, String data){
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        int cmp = data.compareTo(root.data);
+
+        if (cmp < 0) {
+            root.left = insertBST(root.left, data);
+        } else if (cmp > 0) {
+            root.right = insertBST(root.right, data);
+        }
+
+        return root;
+    }
 }
 public class GenericsKbBSTApp {
     
